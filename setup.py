a89a195b9fa3e313afe11306eef35994ce6168fa
Philip Horger <campadrenalin@gmail.com>
2019-03-13 19:43:27 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pmast',
+    version='0.1',
+    description='Pattern Matching for the Python AST',
+    url='https://github.com/campadrenalin/pmast',
+    author='Philip Horger',
+    author_email='campadrenalin@gmail.com',
+    packages=['pmast'],
+    zip_safe=True,
+)
