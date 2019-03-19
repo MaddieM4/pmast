ab36f5a653ce1d1c2608453ff067291b50686da2
Philip Horger <campadrenalin@gmail.com>
2019-03-19 08:34:35 -0700

    Use MIT license

--- setup.py
+++ setup.py
@@ -8,5 +8,13 @@ setup(
     author='Philip Horger',
     author_email='campadrenalin@gmail.com',
     packages=['pmast'],
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: OS Independent',
+    ],
     zip_safe=True,
 )
